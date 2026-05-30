bd92ec7b1f52f06ed0b625b5542ca5e92c194c52
Tanisha <officialtanisha20@gmail.com>
2026-05-30 15:22:51 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -29,4 +29,4 @@ def predict(comment: Comment):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
